60ddbb4e428918231bf0f3fd16afd9d92c3783b1
Muzammilazam22 <Muzammil.azam88@gmail.com>
2021-11-08 16:27:07 +0500

    text change

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,11 +1,11 @@
-import logo from '../../images/svg-1.svg';
+import logo from "../../images/svg-1.svg";
 export const homeObjOne = {
   id: "about",
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
   topLine: "Premium Bank",
-  headline: "Unlimited Transaction with zero fees",
+  headline: "limited Transaction with zero fees",
   description:
     "Get access to our exclusive app that allows you to send unlimited transaction without getting charged any fees.",
   buttonLabel: "Get started",
